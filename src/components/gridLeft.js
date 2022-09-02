a7c230b7b22a6c8f6464619368c39bfc4c7a8d76
puzon44 <pathakpuzon@gmail.com>
2022-09-02 21:27:21 +0545

    Grid template

---
+++ src/components/gridLeft.js
@@ -0,0 +1,66 @@
+import React from "react";
+import Images from "../images/images";
+
+export default function GridLeft() {
+  return (
+    <>
+      <div className="container">
+        <div className="container__left">
+          <div className="items-1">
+            <img src={Images[0].Image1} alt="sds" className="first-child" />
+            <div className="image__description">
+              <h3 className="number-overlap">#1</h3>
+              <p className="text-overlap">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+          </div>
+          <div className="items-2">
+            <img src={Images[1].Image2} alt="sds" />
+            <div className="image__description">
+              <h3 className="number-overlap">#2</h3>
+              <p className="text-overlap">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+          </div>
+          <div className="items-3">
+            <img src={Images[2].Image3} alt="sds" />
+            <div className="image__description">
+              <h3 className="number-overlap">#3</h3>
+              <p className="text-overlap">
+                Lorem, ipsum dolor. 
+              </p>
+            </div>
+          </div>
+          <div className="items-4">
+            <img src={Images[3].Image4} alt="sds" />
+            <div className="image__description">
+              <h3 className="number-overlap">#4</h3>
+              <p className="text-overlap">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+          </div>
+          <div className="items-5">
+            <img src={Images[4].Image5} alt="sds" />
+            <div className="image__description">
+              <h3 className="number-overlap">#5</h3>
+              <p className="text-overlap">
+                Lorem, ipsum dolor.
+              </p>
+            </div>
+          </div>
+          <div className="items-6">
+            <img src={Images[5].Image6} alt="sds" />
+            <div className="image__description">
+              <h3 className="number-overlap">#6</h3>
+              <p className="text-overlap">lorem3</p>
+            </div>
+          </div>
+        </div>
+        <div className="container__right"></div>
+      </div>
+    </>
+  );
+}
---
+++ src/images/images.js
@@ -0,0 +1,11 @@
+const Images = [{Image1: require("./image-1.jpg")},
+  {Image2: require("./image2.jpg")},
+  {Image3: require("./image3.jpg")},
+  {Image4: require("./image4.jpg")},
+  {Image5: require("./image5.jpg")},
+  {Image6: require("./image6.jpg")},
+];
+
+export default Images;
+
+
